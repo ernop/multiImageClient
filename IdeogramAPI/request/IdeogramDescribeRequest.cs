55d59ba1fe7907bdcd3bbe89e3b399f867d35795
Ernie <ernestfrench@gmail.com>
2025-09-26 00:44:03 -0700

    ideogram describe

---
+++ IdeogramAPI/request/IdeogramDescribeRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace IdeogramAPIClient
+{
+    public class IdeogramDescribeRequest
+    {
+        public byte[] ImageFile { get; set; }
+        public string DescribeModelVersion { get; set; };
+    }
+}
